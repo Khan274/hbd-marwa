aaa6957d531699f970f48e5e18967788423a8cab
Khan274 <67997867+Khan274@users.noreply.github.com>
2025-11-09 15:13:34 +0500

    Update HappyBirthday.jsx

--- src/app/components/HappyBirthday.jsx
+++ src/app/components/HappyBirthday.jsx
@@ -220,7 +220,7 @@ export default function HappyBirthday({ onNext }) {
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ delay: 1.3 }}
                 >
-                    Aiman<span className="text-white">💕</span>
+                    Marwa<span className="text-white">💕</span>
                 </motion.h2>
 
                 <motion.div
